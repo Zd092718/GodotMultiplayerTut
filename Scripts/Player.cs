323fc5b2d9c40616f49224b98224f615393a4150
Zack Dowd <zdowd2796@gmail.com>
2024-03-26 16:45:24 -0600

    refactored and camera working

--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -3,23 +3,38 @@ using System;
 
 public partial class Player : CharacterBody2D
 {
+    [ExportGroup("References")]
     [Export] public AnimatedSprite2D animSprite;
+    [Export] public PackedScene playerCamera;
+    [Export] public Vector2 cameraTransform;
     
+    [ExportGroup("Movement")]
     [Export] public float moveSpeed = 300;
     [Export] public float gravity = 30;
     [Export] public float jumpStrength = 600;
     [Export] public int maxJumps = 1;
 
     public Vector2 initialSpriteScale;
-    int jumpCount = 0;
+    private int jumpCount = 0;
+    private Camera2D cameraInstance;
+    private Vector2 newCamGP;
+    private Vector2 newVel;
+    
     public override void _Ready()
     {
         initialSpriteScale = animSprite.Scale;
         animSprite.AnimationFinished += HandleAnimationFinished;
+        SetupCamera();
     }
+
+    public override void _Process(double delta)
+    {
+        UpdateCameraPos();
+    }
+
     public override void _PhysicsProcess(double delta)
     {
-        Vector2 newVel = Velocity;
+        newVel = Velocity;
         float horizontalInput = (
             Input.GetActionStrength(GameConstants.INPUT_MOVE_RIGHT) 
             - Input.GetActionStrength(GameConstants.INPUT_MOVE_LEFT)
@@ -28,12 +43,44 @@ public partial class Player : CharacterBody2D
         newVel.X = horizontalInput * moveSpeed;
         newVel.Y += gravity;
 
+        HandleMovementState();
+
+        Velocity = newVel;
+
+        
+        MoveAndSlide();
+
+
+        FaceMovementDirection(horizontalInput);
+        
+    }
+    private void HandleMovementState()
+    {
+
         bool isFalling = Velocity.Y > 0.0 && !IsOnFloor();
         bool isJumping = Input.IsActionJustPressed(GameConstants.INPUT_JUMP) && IsOnFloor();
         bool isDoubleJumping = Input.IsActionJustPressed(GameConstants.INPUT_JUMP) && isFalling;
         bool isJumpCancelled = Input.IsActionJustReleased(GameConstants.INPUT_JUMP) && Velocity.Y < 0;
         bool isIdle = IsOnFloor() && Mathf.IsZeroApprox(Velocity.X);
         bool isWalking = IsOnFloor() && !Mathf.IsZeroApprox(Velocity.X);
+        
+        if (isJumping)
+        {
+            animSprite.Play(GameConstants.ANIM_JUMP_START);
+        } else if (isDoubleJumping)
+        {
+            animSprite.Play(GameConstants.ANIM_DOUBLE_JUMP_START);
+        } else if (isWalking)
+        {
+            animSprite.Play(GameConstants.ANIM_WALK);
+        } else if (isFalling)
+        {
+            animSprite.Play(GameConstants.ANIM_FALL);
+        } else if (isIdle)
+        {
+            animSprite.Play(GameConstants.ANIM_IDLE);
+        }
+
         
         if (isJumping)
         {
@@ -54,28 +101,9 @@ public partial class Player : CharacterBody2D
         {
             jumpCount = 0;
         }
-        
-        Velocity = newVel;
-
-        
-        MoveAndSlide();
-
-        if (isJumping)
-        {
-            animSprite.Play(GameConstants.ANIM_JUMP_START);
-        } else if (isDoubleJumping)
-        {
-            animSprite.Play(GameConstants.ANIM_DOUBLE_JUMP_START);
-        } else if (isWalking)
-        {
-            animSprite.Play(GameConstants.ANIM_WALK);
-        } else if (isFalling)
-        {
-            animSprite.Play(GameConstants.ANIM_FALL);
-        } else if (isIdle)
-        {
-            animSprite.Play(GameConstants.ANIM_IDLE);
-        }
+    }
+    private void FaceMovementDirection(float horizontalInput)
+    {
 
         if (!Mathf.IsZeroApprox(horizontalInput))
         {
@@ -88,7 +116,21 @@ public partial class Player : CharacterBody2D
                 animSprite.Scale = initialSpriteScale;
             }
         }
-        
+    }
+
+    private void UpdateCameraPos()
+    {
+
+        newCamGP = cameraInstance.GlobalPosition;
+        newCamGP.X = GlobalPosition.X;
+        cameraInstance.GlobalPosition = newCamGP;
+    }
+
+    private void SetupCamera()
+    {
+        cameraInstance = playerCamera.Instantiate<Camera2D>();
+        cameraInstance.GlobalPosition = cameraTransform;
+        GetTree().CurrentScene.CallDeferred("add_child", cameraInstance);
     }
     
     private void HandleAnimationFinished()
